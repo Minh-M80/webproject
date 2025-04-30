d0882021b3254d6f1a0908986687eb246288bbdc
Minh-M80 <dn8513913@gmail.com>
2025-04-30 09:02:56 +0700

    chuyển máy

--- main.js
+++ main.js
@@ -74,4 +74,17 @@ function navigate(page) {
   }
   
   
-  
+ 
+  const backToTopButton = document.getElementById('backToTop');
+
+  window.onscroll = function () {
+    if (window.scrollY > 500) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  };
+
+  backToTopButton.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
